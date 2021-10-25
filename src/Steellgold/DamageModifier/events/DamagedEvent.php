058b90f4ce74a75c33771a6782f57627acd8ad72
Gaëtan H <steellgold0@gmail.com>
2021-10-25 21:08:20 +0200

    Add check if item exist

--- src/Steellgold/DamageModifier/events/DamagedEvent.php
+++ src/Steellgold/DamageModifier/events/DamagedEvent.php
@@ -8,10 +8,17 @@ namespace Steellgold\DamageModifier\events;
 
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
+use pocketmine\Player;
 use Steellgold\DamageModifier\Main;
 
 class DamagedEvent implements Listener {
     public function onInteraction(EntityDamageByEntityEvent $ev){
         $damager = $ev->getDamager(); $victim = $ev->getEntity();
+        if(!$damager instanceof Player) return;
+
+        $itemID = $damager->getInventory()->getItemInHand()->getId(); $itemDG = $damager->getInventory()->getItemInHand()->getDamage();
+        if(!array_key_exists($itemID."-".$itemDG,Main::getInstance()->getItems())) return;
+
+        
     }
 }
